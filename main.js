4d2b71cd9cce4ed37487f372793c6f6c8176c159
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-24 06:59:17 -0300

    Add operations button functionality

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-const numberBtns = document.querySelectorAll(".number");
 const equationDisplay = document.querySelector("#equation");
 const resultDisplay = document.querySelector("#result");
 
+const numberBtns = document.querySelectorAll(".number");
+const operationBtns = Array.from(
+  document.querySelectorAll(".operation")
+).filter((operation) => operation.id !== "equal");
 const pointBtn = document.querySelector("#point");
 
 let currentNumber = 0;
-let equation;
+let n1;
+let equation = 0;
+let operation = null;
 
 numberBtns.forEach((numberBtn) => {
   numberBtn.addEventListener("click", registerNumber);
+});
+
+operationBtns.forEach((operationBtn) => {
+  operationBtn.addEventListener("click", registerOperation);
 });
 
 pointBtn.addEventListener("click", registerPoint);
@@ -17,6 +26,17 @@ function registerNumber(event) {
   currentNumber === 0
     ? (currentNumber = Number(event.target.textContent))
     : (currentNumber += event.target.textContent);
+  equation = currentNumber;
+  updateDisplay();
+}
+
+function registerOperation(event) {
+  if (operation !== null || currentNumber === 0) {
+    return;
+  }
+  n1 = currentNumber;
+  operation = event.target.id;
+  equation += ` ${event.target.textContent} `;
   updateDisplay();
 }
 
@@ -29,6 +49,6 @@ function registerPoint() {
 }
 
 function updateDisplay() {
-  equationDisplay.textContent = currentNumber;
+  equationDisplay.textContent = equation;
   resultDisplay.textContent = currentNumber;
 }
